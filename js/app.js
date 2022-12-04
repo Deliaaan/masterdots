a9e943f9fb0de7ff8fb17891a7d7960e2b63bc02
Deliaaan <delian.ngtmr@gmail.com>
2022-12-04 11:26:16 -0600

    Empezamos con el js del bueno

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-alert("ventana horripilante");
+/* Comprobacion de formularios */ 
+
+/*const nickinput = document.getElementById("nick");
+console.log(nickinput.nodeType);
+nickinput.value = "nickmane";
+console.log(nickinput.value.nodeType);
+
+
+//capturar el valor de select
+
+const tamanoinput = document.getElementById('tamano');
+console.log(tamanoinput.value);
+console.log(tamanoinput.options[tamanoinput.selectedIndex].text)*/
+
+function test(){
+    console.log("Evento sobre click")
+}
