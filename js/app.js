c41757b08e88178a79f810f28ce8e3625ad0b9ba
Deliaaan <delian.ngtmr@gmail.com>
2022-12-28 17:29:21 -0600

    Avance antes del juego principal

--- js/app.js
+++ js/app.js
@@ -1,27 +1,26 @@
-/* Comprobacion de formularios */ 
+//Inicializacion de objetos
+const nickInput = document.getElementById("nick");
+const tamanoInput = document.getElementById("tamano");
+const formEntrada = document.getElementById("formEntrada");
+const error = document.getElementById("error");
 
-/*const nickinput = document.getElementById("nick");
-console.log(nickinput.nodeType);
-nickinput.value = "nickmane";
-console.log(nickinput.value.nodeType);
-
-
-//capturar el valor de select
-
-const tamanoinput = document.getElementById('tamano');
-console.log(tamanoinput.value);
-console.log(tamanoinput.options[tamanoinput.selectedIndex].text)
-
-function test(){
-    console.log("Evento sobre click")
-}*/
-
-// INICIALIZACION DE VARIABLES
-
-const botonJugar = document.getElementById("jugar");
-
-function comprobarFormulario(){
-    console.log("Hola pusssssy jajkad")
+//Funciones de evento 
+function comprobarForm(event){
+    //Comprobar
+    if (nickInput.value.match(/(?<!\S)[0-9]/))
+    {
+        nickInput.focus();
+        event.preventDefault();
+        error.innerText = "Tu nick no puede comenzar con un numero";
+        return false;
+    }else if (tamanoInput.value == "0"){
+        tamanoInput.focus();
+        event.preventDefault();
+        error.innerText = "Se debe seleccionar un tamano de panel";
+        return false;
+    }
+    return false;
 }
 
-botonJugar.addEventListener('click', comprobarFormulario);
+//Inicion de carga de eventos
+formEntrada.addEventListener('submit', comprobarForm);
